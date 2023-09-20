554980a4a54a4da5b2926d864dcde8d7b25aa7c5
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-09-19 21:26:11 -0400

    Add commands endpoints

---
+++ ToDosApi/Controllers/ToDoController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDosApi.Commands;
+using ToDosApi.Exceptions;
+using ToDosApi.Services;
+
+namespace ToDosApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ToDoController : ControllerBase
+{
+    private readonly IToDoCommandHandler _toDoCommandHandler;
+
+    public ToDoController(IToDoCommandHandler toDoCommandHandler)
+    {
+        _toDoCommandHandler = toDoCommandHandler;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> CreateToDoAsync(CreateToDo command)
+    {
+        await _toDoCommandHandler.HandleAsync(command);
+        return Ok();
+    }
+    
+    [HttpDelete]
+    public async Task<ActionResult> RemoveToDoAsync(RemoveToDo command)
+    {
+        try
+        {
+            await _toDoCommandHandler.HandleAsync(command);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        
+        return Ok();
+    }
+    
+    [HttpPut]
+    public async Task<ActionResult> CompleteToDoAsync(CompleteToDo command)
+    {
+        try
+        {
+            await _toDoCommandHandler.HandleAsync(command);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        return Ok();
+    }
+    
+    [HttpPut]
+    [Route("/Name")]
+    public async Task<ActionResult> UpdateToDoAsync(UpdateToDo command)
+    {
+        try
+        {
+            await _toDoCommandHandler.HandleAsync(command);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        return Ok();
+    }
+}
