17d6c22c41b661107323e086ee772f1315a9f126
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-09-23 14:30:21 -0400

    Feat: Edit and delete to do

--- to-dos-app/src/components/ToDoListItem.tsx
+++ to-dos-app/src/components/ToDoListItem.tsx
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Todo } from '../types/type';
-import { Button, Checkbox, Input, Table, Text, Tooltip } from '@mantine/core';
+import { Button, Checkbox, Input, Table, Text, TextInput, Tooltip } from '@mantine/core';
 import { IconPencil, IconTrash } from '@tabler/icons-react';
 import { useAppDispatch } from '../store/store';
-import { completeTodo } from '../store/actionsCreator';
+import { completeTodo, editTodo, removeTodo } from '../store/actionsCreator';
 
 export const ToDoListItem = ({ todo } : { todo: Todo }) => {
   const dispatch = useAppDispatch();
 
   const [checked, setChecked] = useState(false);
+
+  const handleDeleteTodo = () => {
+    dispatch(removeTodo(todo));
+    window.location.reload();
+  }
+
+  const [editedTodo, setEditedTodo] = useState("");
+
+  const handleEditTodo = (event : any) => {
+    dispatch(editTodo({...todo, Task: editedTodo}));
+    window.location.reload();
+  }
+
+  const handleEditInputChange = (event: any) => {
+    event.preventDefault();
+    setEditedTodo(event.target.value);
+  };
 
   return (
     <Table.Tr key={todo.Id}>
@@ -26,7 +43,6 @@ export const ToDoListItem = ({ todo } : { todo: Todo }) => {
         multiline 
         w={350} withArrow
         transitionProps={{ duration: 300 }}
-        color="blue" 
         position="top-start"
         >
          <Text size='md' mt='sm'> { todo.Task } </Text>
@@ -34,17 +50,26 @@ export const ToDoListItem = ({ todo } : { todo: Todo }) => {
     </Table.Td>
 
     <Table.Td>
-        <Button variant="filled" rightSection={<IconTrash size={14} />} color="rgba(212, 61, 61, 1)" radius="lg">
+        <Button variant="filled" rightSection={<IconTrash size={14} />} color="rgba(212, 61, 61, 1)" radius="lg" onClick={handleDeleteTodo}>
         Delete
         </Button>
     </Table.Td>
 
     <Table.Td>
-        <Input rightSection={<IconPencil size={14} />}
-        placeholder="Edit task"
-        radius="xl"
-        onSubmit={() => (console.log)}
-        />
+        <form onSubmit={handleEditTodo}>
+        <Tooltip disabled={todo.CompletedDateTime === null} label={`You can't edit a completed task.`} 
+          withArrow 
+          transitionProps={{ duration: 300 }} 
+          position="top-start">
+            <TextInput
+              disabled={todo.CompletedDateTime !== null}
+              rightSection={<IconPencil size={14} />}
+              placeholder="Edit task"
+              radius="xl"
+              value={editedTodo}
+              onChange={handleEditInputChange}/>
+        </Tooltip>
+        </form>
         </Table.Td>
     </Table.Tr>
   )
