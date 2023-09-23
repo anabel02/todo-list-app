94dfec23e52aa5518ba9b8b0ac62e34700c8ba52
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-09-23 01:11:27 -0400

    Feat: Add to do

--- to-dos-app/src/components/AddToDo.tsx
+++ to-dos-app/src/components/AddToDo.tsx
@@ -1,27 +1,40 @@
 import { TextInput, Button, Group, Box } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { useState } from 'react';
+import { useAppDispatch } from '../store/store';
+import { addTodo } from '../store/actionsCreator';
 
-export const AddToDo : React.FC<{callback: (value: string) => void}>= ({ callback }) => {
-    const form = useForm({
-        initialValues: {
-          task: ''
-        }, validate: {
-            task: (value) => (value.trim().length > 0 ? null : `Task text mustn't be empty`),
-          },
-        });
+export const AddToDo = () => {
+  const dispatch = useAppDispatch();
 
-        return (
-            <Box maw={340} h={100} mx="auto">
-              <form onSubmit={form.onSubmit(({task}) => callback(task))}>
-                <TextInput
-                  placeholder="New To Do..."
-                  {...form.getInputProps('task')}
-                />
-        
-                <Group justify="flex-end" mt="md">
-                  <Button type="submit">Add To Do</Button>
-                </Group>
-              </form>
-            </Box>
-          );
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim().length == 0){
+      console.log(`Task text mustn't be empty`)
+      return;
+    };
+    dispatch(addTodo(newTodo, new Date()));
+    setNewTodo("");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setNewTodo(e.target.value);
+  };
+
+  return (
+    <Box maw={340} h={100} mx="auto">
+      <form onSubmit={handleAddTodo}>
+        <TextInput
+          onChange={handleInputChange}
+          value={newTodo}
+        />
+
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Add To Do</Button>
+        </Group>
+      </form>
+    </Box>
+  );
 }
