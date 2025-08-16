e0dadb435d971239850d6adb64fbcda076f4e8b2
Anabel Benítez <anabelbenitezglez@gmail.com>
2025-08-16 18:29:25 -0400

    Remove react client

--- ToDoListApp.AppHost/Program.cs
+++ ToDoListApp.AppHost/Program.cs
@@ -9,17 +9,13 @@ var mysql = builder.AddMySql("mysql", password, 3306).WithLifetime(ContainerLife
 var todoAppDb = mysql.AddDatabase("todo-db");
 var authServiceDb = mysql.AddDatabase("auth-db");
 
-builder.AddProject<ToDoListApp_AuthService>("auth-service")
-    .WaitFor(authServiceDb).WithReference(authServiceDb, "AuthServiceDb");
+var authService = builder.AddProject<ToDoListApp_AuthService>("auth-service")
+    .WaitFor(authServiceDb)
+    .WithReference(authServiceDb, "AuthServiceDb");
 
 var todoListBackend = builder
     .AddProject<ToDoListApp_Api>("to-do-list-backend")
     .WaitFor(todoAppDb)
     .WithReference(todoAppDb, "ToDoListAppDb");
-
-var reactClient = builder
-    .AddNpmApp("to-do-list-react", "../to-dos-app")
-    .WithHttpEndpoint(targetPort: 3000)
-    .WithEnvironment("REACT_APP_API_URL", todoListBackend.GetEndpoint("http"));
 
 builder.Build().Run();
